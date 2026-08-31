07cfac581da67ea31f193f06bf5abab170678990
SARMAN ROY CHOWDHURY <sarmanroychowdhury@gmail.com>
2026-08-31 10:30:09 +0530

    Update memoaisation.py

--- memoaisation.py
+++ memoaisation.py
@@ -1,14 +1,23 @@
-def fib (n, memo ={}):
-    if n<=1:
+# Function to calculate the Fibonacci number using memoization
+def fib(n, memo={}):
+    # Base case: Fibonacci of 0 is 0 and Fibonacci of 1 is 1
+    if n <= 1:
         return n
-    
+
+    # Check if the Fibonacci number is already calculated
     if n in memo:
         return memo[n]
-    
-    memo[n] = fib (n-1, memo) + fib (n-2 , memo )
+
+    # Calculate Fibonacci number recursively and store it in memo
+    memo[n] = fib(n - 1, memo) + fib(n - 2, memo)
+
+    # Return the calculated Fibonacci number
     return memo[n]
 
-n = int(input("Enter number:"))
 
+# Take the number of terms from the user
+n = int(input("Enter number: "))
+
+# Print the first n Fibonacci numbers
 for i in range(n):
-    print(fib(i), end = " ")
+    print(fib(i), end=" ")
